633f5ddf8f164106f286f6a9346581f08ecb7c5c
SebastianMitco <70155015+SebastianMitco@users.noreply.github.com>
2020-09-29 21:08:06 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+var API_KEY = "c941a4aa134e49e4fb4818bb927fe033";
+
+var getWeather = function (event) {
+  var city = document.querySelector("#weather").value;
+  if (city.length > 2) {
+    // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+    requestUrl += city + "&appid=" + API_KEY + "&units=metric";
+
+    fetch(requestUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (result) {
+        console.log("weather in ", result.name);
+        var temp = Math.ceil(result.main.temp);
+        var text = "Weather in " + city + " is " + temp + " degrees.";
+        document.querySelector("#result").innerText = text;
+      });
+  }
+};
+
+document.querySelector("#search").addEventListener("click", getWeather);
